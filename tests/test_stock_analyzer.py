cd87c39a7c6cda0c3d1c829e9b4acc1bbbf858af
tingyu00 <zoezhang0408@gmail.com>
2021-03-05 10:34:15 -0800

    Add unit tests

--- tests/test_stock_analyzer.py
+++ tests/test_stock_analyzer.py
@@ -2,6 +2,7 @@ from stock_analyzer import __version__
 from stock_analyzer import stock_analyzer
 from pytest import raises
 import pandas as pd
+import numpy as np
 
 
 def test_version():
@@ -82,10 +83,12 @@ def test_SummaryStats():
 
 
 def test_movingAverage():
+
+    # normal test
     source = pd.DataFrame(
         data=[
-            [1, 2, 3, 4, 5],
-            [1, 2, 3, 4, 5],
+            ['1', 2, 3, 4, 5],
+            [1, '2.222222', 3, 4, 5],
             [1, 2, 3, 4, 5],
             [1, 2, 3, 4, 5],
             [1, 2, 3, 4, 5],
@@ -104,6 +107,77 @@ def test_movingAverage():
     assert df_movingAverage.columns.to_list()[0] == "movingAverage1"
     assert list(df_movingAverage["movingAverage1"].values) == [1, 1, 1, 1, 1, 1, 1, 1]
 
+    # single string test
+    data_0 = 'this is a string'
+    with raises(ValueError) as execinfo_0:
+        stock_analyzer.movingAverage(data_0,3, ["movingAverage"])
+    assert (
+        str(execinfo_0.value)
+        == "Your input data cannot be converted to a pandas dataframe."
+    )
+
+    # panda NA test
+    data_1 = pd.DataFrame(
+        data=[
+            [1, 2, 3, 4, 5],
+            [pd.NA, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+        ],
+        columns=["e", "2", "3", "4", "5"],
+    )
+    with raises(TypeError) as execinfo_1:
+        stock_analyzer.movingAverage(data_1,3, ["movingAverage" + name for name in data_1.columns])
+    assert (
+        str(execinfo_1.value)
+        == "Type of Column e isn't a string or a number "
+    )
+
+    # numpy NaN test
+    data_2 = pd.DataFrame(
+        data=[
+            [1, 2, 3, 4, 5],
+            [np.NaN, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [np.NaN, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [np.NaN, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+        ],
+        columns=["e", "2", "3", "4", "5"],
+    )
+    with raises(ValueError) as execinfo_2:
+        stock_analyzer.movingAverage(data_2,3, ["movingAverage" + name for name in source.columns])
+    assert (
+        str(execinfo_2.value)
+        == "Column e has Nan at [1] [3] [5] "
+    )
+
+    # String test
+    data_3 = pd.DataFrame(
+        data=[
+            [1, 2, 3, 4, 5],
+            ["p", 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+        ],
+        columns=["e", "2", "3", "4", "5"],
+    )
+    with raises(ValueError) as execinfo_3:
+        stock_analyzer.movingAverage(data_3,3, ["movingAverage" + name for name in source.columns])
+    assert (
+        str(execinfo_3.value)
+        == "Column e can't be converted to floating point"
+    )
 
 test_SummaryStats()
 test_movingAverage()
